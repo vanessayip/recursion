f4833d86e4b35a9237e73c8059fc7f7ec2b75ba0
alfonso <alfonso@hansacanada.com>
2017-09-17 21:49:55 -0700

    finished

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,46 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  //document.body , element.childNodes, element.classList
+  var results = [];
+  // var allElements = [];
+  // var flattenedElements = []; 
+  var allElements = flatten([], document.body);
+  console.log(allElements)
+  // debugger
+  for (var i = 0; i < allElements.length; i++) {
+    var element = allElements[i];
+    if (element.classList) {
+      if (element.classList.contains(className)) {
+        results.push(element);
+      }
+    }
+  }
+
+  // console.log(results);
+  return results;
+  // return document.getElementsByClassName(className);
+  // return results;
 };
+
+
+var flattenedElements = []; 
+var flatten = function(flattenedElements, node) {
+  // debugger
+  flattenedElements.push(node);
+  if (node.childNodes.length === 0 ) {
+    // console.log(flattenedElements.concat(node))
+    // flattenedElements.push(node)
+    // console.log(flattenedElements)
+    // return flattenedElements;
+  } else {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      // console.log(node.childNodes[i]);
+      flatten(flattenedElements, node.childNodes[i]);
+    }
+  }
+  return flattenedElements;
+}
